e36223ae9160c2dbcc0e8052ffeab22e1842c5cb
charcalope <47309188+charcalope@users.noreply.github.com>
2024-08-16 12:56:58 -0700

    refactor surface pipeline and begin work on mask export pipeline

---
+++ models/surfaces/extract_surfaces.py
@@ -0,0 +1,91 @@
+import skimage
+from surface_faces import mask_maker
+import re
+import os
+import numpy as np
+import csv
+
+def mask_controller(filepath):
+    img = skimage.io.imread(filepath)
+    mask_gen = mask_maker(img,
+                          mask_min=8, mask_max=78)
+    yield from mask_gen
+
+
+def directory_crawler(sample_directory):
+    def extract_sample_name(filepath: str, pattern):
+        file_str = filepath.replace("/", "+")  # makes regex easier to read
+        match = re.search(pattern=pattern, string=file_str)
+        yield filepath, str(f"{match.group(1)}_{match.group(2)}")
+
+    # Change to the parent of the parent directory
+    os.chdir(os.path.join(os.path.pardir, os.path.pardir))
+
+    # Specify the root directory to start the walk
+    root_directory = str(f"data/{sample_directory}")
+
+    # Walk through the directory tree
+    for dirpath, dirnames, filenames in os.walk(root_directory):
+        # List all files in the current directory
+        for filename in filenames:
+            if ".DS_Store" not in filename:
+                path = str(f"{os.path.join(dirpath, filename)}")
+                yield from extract_sample_name(path, pattern=r"([^+]+)?\+([^+]+)?\.(png|jpg|jpeg)")
+
+
+def setup_directory_writer(output_dir):
+    def directory_writer(directory_path):
+        while True:
+            # ingest
+            row_data = yield
+
+            # write
+            with open(directory_path, 'a', newline='') as directory_file:
+                dir_fieldnames = ['original_file', 'sample_name', 'mask_filepath']
+                dir_writer = csv.DictWriter(directory_file, fieldnames=fieldnames)
+                dir_writer.writerow(row_data)
+
+    filepath1 = str(f"{output_dir}/directory.csv")
+    with open(filepath1, 'w', newline='') as csvfile:
+        fieldnames = ['original_file', 'sample_name', 'mask_filepath']
+        writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+        writer.writeheader()
+
+    writer_generator = directory_writer(filepath1)
+    writer_generator.__next__()
+    return writer_generator
+
+
+# makes a directory.csv file and a nested directory
+# the subdirectory mask_data contains all .txt files with saved arrays
+def mask_pipeline_controller(sample_directory, output_directory):
+    # make subdirectory to hold outputted masks
+    os.mkdir(str(f"{output_directory}/mask_data"))
+
+    # make csv directories
+    pipeline_writer = setup_directory_writer(output_directory)
+
+    # crawl sample directory, generate masks for one sample at a time
+    for filename, mask_file_prefix in directory_crawler(sample_directory):
+        # Instantiate surface separation pipeline for this mask
+        sample_mask_generator = mask_controller(filename)
+
+        mask_index = 0
+        for new_mask in sample_mask_generator:
+            # write mask to directory
+            mask_save_filepath = str(f"{output_directory}/mask_data/{mask_file_prefix}_mask_{mask_index}.txt")
+            np.savetxt(mask_save_filepath, new_mask)
+
+            # record new file in directory
+            pipeline_writer.send(
+                {
+                    "original_file": filename,
+                    "sample_name": mask_file_prefix,
+                    "mask_filepath": mask_save_filepath
+                }
+            )
+
+            mask_index += 1
+
+
+
--- models/surfaces/surface_faces.py
+++ models/surfaces/surface_faces.py
@@ -1,27 +1,20 @@
 import random
 
-import matplotlib.pyplot as plt
 import numpy as np
-import skimage.io
 from skimage.filters import sato
 from skimage.morphology import convex_hull_image
-from skimage.segmentation import flood, flood_fill
-
-
-def sample_random_point(width, height):
-    """
-    Randomly samples a point from an image with given width and height.
-    """
-    x = random.randint(0, width - 1)
-    y = random.randint(0, height - 1)
-    return x, y
+from skimage.segmentation import flood
 
 
 def point_within_mask(mask):
-    """
-    :param mask: boolean mask array
-    :return: (x,y) point within mask
-    """
+    def sample_random_point(width, height):
+        """
+        Randomly samples a point from an image with given width and height.
+        """
+        x = random.randint(0, width - 1)
+        y = random.randint(0, height - 1)
+        return (x, y)
+
     height, width = mask.shape
 
     random_pt = sample_random_point(width, height)
@@ -30,34 +23,7 @@ def point_within_mask(mask):
     return random_pt
 
 
-def plot_image(img):
-    # Plot the image
-    plt.imshow(img, cmap='gray')
-    plt.axis('off')  # Turn off axis labels
-    plt.show()
-
-
-def mask_maker(filepath, mask_min, mask_max, stop_condition=12, verbalize=False):
-    """
-    Generates a convex hull over the drawn shape in prepared sample
-    Applies Sato ridge operator to create sufficient gradient
-    Samples random points within mask, and attempts flood-fill
-
-    If flood-fill area as % of convex hull area is in range (mask_min, mask_max)
-    subtract mask area from convex hull, then yield mask
-
-    Will stop independently if remaining % "stop_condition" is met
-
-    :param filepath: image to load and process
-    :param mask_min: min % to pass
-    :param mask_max: max % to pass
-    :param stop_condition: remaining % to stop iteration
-    :param verbalize: communicate extra for debugging
-
-    :return: generator yielding flood masks
-    """
-    # Load image and convex hull of drawn shape
-    image = skimage.io.imread(filepath)
+def mask_maker(image, mask_min, mask_max, stop_condition=12, max_fails=50, verbalize=False):
     chull = convex_hull_image(image)
 
     # Sato Ridge Operator
@@ -66,7 +32,9 @@ def mask_maker(filepath, mask_min, mask_max, stop_condition=12, verbalize=False)
     # Track percentage unfilled / unmasked
     unfilled_percentage = 100
 
-    while unfilled_percentage > stop_condition:  # Generate a flood mask from random point
+    consecutive_fails = 0
+    while (unfilled_percentage > stop_condition) and (
+            consecutive_fails < max_fails):  # Generate a flood mask from random point
         random_pt = point_within_mask(chull)
 
         # Flood fill
@@ -95,89 +63,13 @@ def mask_maker(filepath, mask_min, mask_max, stop_condition=12, verbalize=False)
             # Communicate
             if verbalize:
                 print(f"Pass at: {flood_percentage}")
-                plot_image(flood_fill(result, (random_pt[1], random_pt[0]), new_value=110, tolerance=10))
 
+            consecutive_fails = 0
             yield flood_mask
         else:
             # Communicate
             if verbalize:
                 print(f"Fail at: {flood_percentage}")
-            yield
 
-
-def mask_controller(filepath, img_output=False):
-    """
-    Runs generator mask_gen
-    Tracks consecutive fails, stops iteration at 50
-
-    Creates an image copy, recolors with flood fill from masks
-    Returns this along with metadata on mask count, coloring used
-
-    :param filepath: image to process
-    :param img_output: returns image only, or None if no masks generated
-    :return: image and metadata
-    """
-    mask_gen = mask_maker(filepath,
-                          mask_min=11, mask_max=78)
-
-    masks = []
-    consecutive_fails = 0
-    while consecutive_fails < 50:
-        try:
-            response = mask_gen.__next__()
-            if response is not None:  # success, is a mask
-                consecutive_fails = 0
-                masks.append(response)
-            else:
-                consecutive_fails += 1  # fail
-        except StopIteration:
-            exit()
-
-    img = skimage.io.imread(filepath)
-
-    # Create a copy of the image to modify
-    recolored_img = img.copy()
-    secondary_colors = []
-
-    # Sort masks by area (count of True pixels)
-    masks.sort(key=lambda m: np.sum(m), reverse=True)
-
-    # Recolor the largest mask in red and the others randomly
-    for i, m in enumerate(masks):
-        if i == 0:
-            # Largest mask, color it red
-            color = np.array([255, 0, 0], dtype=np.uint8)
-        else:
-            # Other masks, color them randomly
-            color = np.random.randint(0, 256, size=(3,), dtype=np.uint8)
-            secondary_colors.append(color)
-
-        # If the image is grayscale, convert to RGB
-        if len(recolored_img.shape) == 2:  # grayscale image
-            recolored_img = np.stack([recolored_img] * 3, axis=-1)
-
-        # Apply the color to the mask region in the image
-        recolored_img[m] = color
-
-    if img_output:  # do not return metadata with image
-        if len(masks) == 0:
-            return None
-        else:
-            return recolored_img
-
-    # return results
-    if len(masks) > 0:
-        return {
-            "status": "pass",
-            "original_file": filepath,
-            "masks_n": len(masks),
-            "primary_color": [255,0,0],
-            "secondary_colors": secondary_colors,
-        }
-    else:
-        return {
-            "status": "fail",
-            "original_file": filepath,
-        }
-
+            consecutive_fails += 1
 
